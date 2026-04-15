6849064d365359622eb04e88bd4109062e4c306a
Drif <yanis.drif@efrei.net>
2026-04-15 15:36:58 +0200

    Update cityalert-backend

--- cityalert-backend/server.js
+++ cityalert-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express"); // creer un server avec express
 const mysql = require("mysql2"); // module pour se connecter à une base de données MySQL
 const bcrypt = require("bcrypt"); // module pour hasher les mots de passe avant de les stocker dans la DB pour des raisons de sécurité
+const multer = require('multer');
 require('dotenv').config(); //sert a stocker des variable sensibles dans un fichier .env et les charger dans process.env
 const jwt = require("jsonwebtoken");
 
@@ -12,10 +13,52 @@ const db = mysql.createConnection({ //creer une connexion a la DB en utilisant l
   database: process.env.DB_NAME
 });
 
+
 const app = express(); // creer le serveur express
 app.use(express.json()); // Middleware pour parser le corps de la requete en JSON
+app.use('/uploads', express.static('uploads'));
 
 
+function authMiddleware(req, res, next) { // Middleware pour verifier le token JWT dans les requetes qui necessitent une authentification
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; // extraire le token de l'en-tete Authorization
+  if (!token) {
+    return res.status(401).json({ error: 'Token manquant' });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token invalide' });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+function roleMiddleware(requiredRole) {
+  return (req, res, next) => {
+    const userId = req.user.userId; // recuperer le role de l'utilisateur a partir du token JWT
+    db.query('SELECT role FROM users WHERE id = ?', [userId], (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error fetching user role' });
+      }
+      if (!results.length || results[0].role !== requiredRole) {
+        return res.status(403).json({ error: 'Insufficient privileges' });
+      }
+      next();
+    });
+  };
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({ storage });
 
 app.get('/', (req, res) => { // definir une route pour la racine du serveur pour tester le serveur
   res.send('Hello, World!');
@@ -61,6 +104,10 @@ app.post('/login', (req, res) => {
   }
 
   db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
+
+    if (err) {
+      return res.status(500).json({ error: 'Error fetching user' });
+    }
     if (results.length === 0) {
       return res.status(401).json({ error: 'User introuvable' });
     }
@@ -73,37 +120,183 @@ app.post('/login', (req, res) => {
     const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' }); // generer un token JWT qui contient l'id de l'utilisateur et qui expire dans 1 heure
     res.json({ message: 'Login successful', token });
   });
+
   
 });
 
 
 
 app.get('/reports', (req, res) => {
-  db.query('SELECT * FROM reports', (err, results) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const offset = (page - 1) * limit;
+
+  const sort = req.query.sort;
+
+  const allowedSort = ['created_at', 'priority'];
+  const sortField = allowedSort.includes(sort) ? sort : 'created_at';
+
+  db.query(`SELECT * FROM reports ORDER BY ${sortField} DESC LIMIT ? OFFSET ?`,[limit, offset],(err, results) => {
+      if (err) {
+        return res.status(500).json({ error: 'Error fetching reports' });
+      }
+
+      res.json({ page, limit, count: results.length, results});
+    }
+  );
+});
+
+app.post('/reports', authMiddleware,upload.single('image'), (req, res) => {
+  const { title, description, latitude, longitude } = req.body;
+  const image_url = req.file ? req.file.filename : null;
+  const userId = req.user.userId;
+  
+  if (!title || !description) {
+  return res.status(400).json({ error: 'Champs obligatoires' });
+}
+
+  db.query(
+    'SELECT * FROM reports WHERE latitude = ? AND longitude = ?', [latitude, longitude], (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error checking reports' });
+      }
+
+      if (results.length > 0) {
+        const report = results[0];
+
+        db.query(
+          'UPDATE reports SET report_count = report_count + 1 WHERE id = ?', [report.id], (err) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({ error: 'Error updating report count' });
+            }
+
+            return res.json({ message: 'Signalement déjà existant', reportId: report.id});
+          }
+        );
+      }
+
+      else {
+        db.query('INSERT INTO reports (user_id, title, description, latitude, longitude, image_url, role) VALUES (?, ?, ?, ?, ?, ?, ?)',
+          [userId, title, description, latitude, longitude, image_url, 'user'],
+          
+          (err, results) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({ error: 'Error inserting report' });
+            }
+
+            res.status(201).json({ message: 'Report created', reportId: results.insertId});
+          }
+        );
+      }
+    }
+  );
+});
+
+app.get('/my-reports', authMiddleware, (req, res) => {
+  const userid = req.user.userId;
+  db.query('SELECT * FROM reports WHERE user_id = ?', [userid], (err, results) => {
     if (err) {
-      console.error('Error fetching reports:', err);
-      res.status(500).send('Error fetching reports');
-      return;
+      console.error('Error fetching user reports:', err);
+      return res.status(500).json({ error: 'Error fetching user reports:' });
+    }
+    if (results.length === 0) {
+      return res.json([]);
+    }
+
+    res.json(results);
+  });
+});
+
+
+app.get('/admin/reports', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  const status = req.query.status || 'pending';
+  db.query('SELECT * FROM reports WHERE status = ?', [status], (err, results) => {
+    if (err) {
+      console.error('Error fetching reports by status:', err);
+      return res.status(500).json({ error: 'Error fetching reports by status' });
+    }
+    res.json({count: results.length,reports: results });
+  });
+});
+
+app.put('/admin/reports/:id/approve', authMiddleware,roleMiddleware('admin'), (req, res) => {
+  const reportId = req.params.id;
+  db.query('UPDATE reports SET status = ? WHERE id = ?', ['approved', reportId], (err) => {
+    if (err) {
+      console.error('Error approving report:', err);
+      return res.status(500).json({ error: 'Error approving report' });
+    }
+    res.json({ message: 'Report approved' });
+  });
+});
+
+app.put('/admin/reports/:id/reject', authMiddleware,roleMiddleware('admin'), (req, res) => {
+  const reportId = req.params.id;
+  db.query('UPDATE reports SET status = ? WHERE id = ?', ['rejected', reportId], (err) => {
+    if (err) {
+      console.error('Error rejecting report:', err);
+      return res.status(500).json({ error: 'Error rejecting report' });
+    }
+    res.json({ message: 'Report rejected' });
+  });
+});
+
+app.put('/admin/reports/:id/resolve', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  const reportId = req.params.id;
+  db.query('UPDATE reports SET status = ? WHERE id = ?', ['resolved', reportId], (err) => {
+    if (err) {
+      console.error('Error resolving report:', err);
+      return res.status(500).json({ error: 'Error resolving report' });
+    }
+    res.json({ message: 'Report resolved' });
+  });
+});
+
+app.get('/stats', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  db.query('SELECT COUNT(*) as total FROM reports', (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: 'Error fetching stats' });
+    }
+
+    res.json({
+      totalReports: results[0].total
+    });
+  });
+});
+
+app.get('/stats/status', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  db.query('SELECT status, COUNT(*) AS count FROM reports GROUP BY status', (err, results) => {
+    if (err) {
+      console.error('Error fetching report stats:', err);
+      return res.status(500).json({ error: 'Error fetching report stats' });
     }
     res.json(results);
   });
 });
 
-app.post('/reports', (req, res) => {
-  const { title, description, latitude, longitude,image_url } = req.body; // recuperer les données du corps de la requete
-
-  const user_id = 1; // pour l'instant on utilise un user_id statique, a remplacer par l'id de l'utilisateur connecté dans une version future
-  db.query('INSERT INTO reports (user_id,title, description, latitude, longitude,image_url) VALUES (?, ?, ?, ?, ?, ?)', [user_id,title, description, latitude, longitude,image_url], (err, results) => {
+app.get('/stats/priority', authMiddleware, roleMiddleware('admin'), (req, res) => {
+  db.query('SELECT priority, COUNT(*) AS count FROM reports GROUP BY priority', (err, results) => {
     if (err) {
-      console.error('Error inserting report:', err);
-      res.status(500).send('Error inserting report');
-      return;
+      console.error('Error fetching report stats:', err);
+      return res.status(500).json({ error: 'Error fetching report stats' });
     }
-    else {
-      res.status(201).json({ message: 'Report created', reportId: results.insertId });
-    }
+    res.json(results);
   });
 });
+
+  
+
+
+
+
+
+
+
+
+
 
 
 
